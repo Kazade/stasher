1354a9182acaeaa737407596577cf928e8b5b9ef
Luke Benstead <kazade@gmail.com>
2016-05-08 15:19:25 +0100

    Implement missing Entity methods

--- stasher/entity.cpp
+++ stasher/entity.cpp
@@ -1,2 +1,13 @@
 #include "entity.h"
 
+Entity::Entity(const std::string& kind, int64_t id) {
+    properties_[KEY_PROPERTY] = Key(kind, id);
+}
+
+Entity::Entity(const std::string& kind, const std::string& name) {
+    properties_[KEY_PROPERTY] = Key(kind, name);
+}
+
+void Entity::set(const std::string& property, Property value) {
+    properties_[property] = value;
+}
